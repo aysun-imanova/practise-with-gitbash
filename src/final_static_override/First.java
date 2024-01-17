a960acbc52411865cd14732a0785dfc6911acc88
aysun-imanova <63191090+aysun-imanova@users.noreply.github.com>
2024-01-17 12:35:19 +0400

    10th commit

--- src/final_static_override/First.java
+++ src/final_static_override/First.java
@@ -6,5 +6,14 @@ public class First {
     private void print() {
         System.out.println(number);
     }
+
+    static void eat(int number) {
+        System.out.println("Animal is eating!");
+    }
+
+
+    public void test() {
+        System.out.println("First Class");
+    }
 }
 
--- src/final_static_override/Second.java
+++ src/final_static_override/Second.java
@@ -1,5 +1,39 @@
 package final_static_override;
 
+/**
+ * we can declare private variables again in other inherit class
+ * static fields/methods don't pass to inherit class
+ * private -> default -> protected -> public
+ * we can't override static methods
+ */
 public class Second extends First {
+
+    private int count;
+
+    private void print() {
+        System.out.println(count);
+    }
+
+    static void eat(int count) {
+        System.out.println("Static method in Second class");
+    }
+
+    @Override
+    public void test() {
+        System.out.println("Test method in Second class");
+    }
+
+    public static void main(String[] args) {
+        First first = new First();
+        first.test();
+
+        Second second = new Second();
+        second.test();
+
+        first = second;
+
+        first.test();
+    }
+
 
 }
